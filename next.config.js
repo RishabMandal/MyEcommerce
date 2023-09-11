bdfeb60b6f25634d454f5a905f61edf93022f9a1
Rishab Mandal <102305005+RishabMandal@users.noreply.github.com>
2023-09-11 07:33:37 +0530

    First

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //   webpack: (config, { isServer }) => {
-  //     if (!isServer) {
-  //       // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
-  //       config.resolve.fallback = {
-  //         fs: false,
-  //       };
-  //     }
-
-  //     return config;
-  //   },
+  output: "export",
   env: {
     MONGO_URL:
       "mongodb+srv://Rishab829:Kanchan%401@expresstry.wqhmyb0.mongodb.net/myecommerce?retryWrites=true&w=majority",
