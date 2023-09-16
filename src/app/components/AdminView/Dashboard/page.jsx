0f48be8bc5e9446642801e750f87392bb5af9615
Rishab Mandal <102305005+RishabMandal@users.noreply.github.com>
2023-09-16 17:45:51 +0530

    Optimizing phone view, working on cart and implementation of review started

--- src/app/components/AdminView/Dashboard/page.jsx
+++ src/app/components/AdminView/Dashboard/page.jsx
@@ -7,6 +7,9 @@ const page = () => {
   const [data, setData] = useState([]);
   const [totalPriceDay, settotalPriceDay] = useState(0);
   const [totalOrdersDay, settotalOrdersDay] = useState(0);
+  const [totalPriceTillNow, settotalPriceTillNow] = useState(0);
+  const [totalOrdersTillNow, settotalOrdersTillNow] = useState(0);
+  const date = new Date();
   useEffect(() => {
     axios
       .post("/api/Orders", { email: "all" })
@@ -15,9 +18,27 @@ const page = () => {
   }, []);
   useEffect(() => {
     data.forEach((product) => {
-      settotalPriceDay((prev) => prev + product.price);
+      if (
+        date.toString().substring(0, 15) ===
+        product?.date.toString().substring(0, 15)
+      ) {
+        settotalPriceDay((prev) => prev + product.price);
+      }
     });
-    settotalOrdersDay(data.length);
+    data.forEach((product) => {
+      if (
+        date.toString().substring(0, 15) ===
+        product?.date.toString().substring(0, 15)
+      ) {
+        settotalOrdersDay((prev) => prev + 1);
+      }
+    });
+    // settotalOrdersDay(data.length);
+    data.forEach((product) => {
+      settotalPriceTillNow((prev) => prev + product.price);
+    });
+    settotalOrdersTillNow(data.length);
+    // alert(date.toString().substring(0,15));
   }, [data]);
 
   return (
@@ -26,10 +47,10 @@ const page = () => {
         <div className="text-xl">
           Hello, <a className="font-bold">Admin</a>
         </div>
-        <div className="flex flex-wrap gap-5">
+        <div className="flex flex-wrap gap-6">
           <div>
             <div className="text-xl font-semibold pt-10 pb-5">Orders</div>
-            <div className="flex flex-row">
+            <div className="flex flex-wrap gap-3">
               <div className="bg-white p-10 rounded-xl border shadow-xl text-center">
                 <div className="text-[#121212] text-lg font-bold">TODAY</div>
                 <div className="text-red-700 text-2xl font-bold my-2">
@@ -39,11 +60,20 @@ const page = () => {
                   {totalOrdersDay} order(s) today
                 </div>
               </div>
+              <div className="bg-white p-10 rounded-xl border shadow-xl text-center">
+                <div className="text-[#121212] text-lg font-bold">TOTAL</div>
+                <div className="text-red-700 text-2xl font-bold my-2">
+                  {totalOrdersTillNow}
+                </div>
+                <div className="text-gray-500 text-base">
+                  {totalOrdersTillNow} order(s) till now
+                </div>
+              </div>
             </div>
           </div>
           <div>
             <div className="text-xl font-semibold pt-10 pb-5">Revenue</div>
-            <div className="flex flex-row">
+            <div className="flex flex-wrap gap-3">
               <div className="bg-white p-10 rounded-xl border shadow-xl text-center">
                 <div className="text-[#121212] text-lg font-bold">TODAY</div>
                 <div className="text-red-700 text-2xl font-bold my-2">
@@ -53,12 +83,23 @@ const page = () => {
                   {totalOrdersDay} order(s) today
                 </div>
               </div>
+              <div className="bg-white p-10 rounded-xl border shadow-xl text-center">
+                <div className="text-[#121212] text-lg font-bold">TOTAL</div>
+                <div className="text-red-700 text-2xl font-bold my-2">
+                  ₹ {totalPriceTillNow}
+                </div>
+                <div className="text-gray-500 text-base">
+                  {totalOrdersTillNow} order(s) till now
+                </div>
+              </div>
             </div>
           </div>
         </div>
         <div>
           <div>
-            <div className="text-xl font-semibold pt-10 pb-5">Most Ordered Products</div>
+            <div className="text-xl font-semibold pt-10 pb-5">
+              Most Ordered Products
+            </div>
             <div className="flex flex-row">
               <div className="bg-white p-10 rounded-xl border shadow-xl text-center">
                 <div className="text-[#121212] text-lg font-bold">TODAY</div>
--- src/components/AdminModal.jsx
+++ src/components/AdminModal.jsx
@@ -7,59 +7,57 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
+  width: "100%",
+  height: "100%",
 };
 
 export default function BasicModal({ open, setOpen }) {
-  //   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
   return (
-    <div>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <div>
-          <div className="flex justify-center items-center min-h-[70vh] gap-5">
-            <Link
-              href="/components/AdminView/Dashboard"
-              onClick={() => setOpen(false)}
-              className="bg-red-600 text-xl p-5 text-white font-bold rounded-xl"
-            >
-              Dashboard
-            </Link>
-            <Link
-              href="/components/AdminView/AvailableProducts"
-              onClick={() => setOpen(false)}
-              className="bg-red-600 text-xl p-5 text-white font-bold rounded-xl"
-            >
-              Products
-            </Link>
-            <Link
-              href="/components/AdminView/AvailableProducts"
-              onClick={() => setOpen(false)}
-              className="bg-red-600 text-xl p-5 text-white font-bold rounded-xl"
-            >
-              Categories
-            </Link>
-            <Link
-              href="/components/AdminView/Orders"
-              onClick={() => setOpen(false)}
-              className="bg-red-600 text-xl p-5 text-white font-bold rounded-xl"
-            >
-              Orders
-            </Link>
-          </div>
+    // <div>
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+      style={style}
+    >
+      <div className="bg-white rounded-xl translate-y-1/2 shadow-xl p-5 w-fit mx-auto">
+        <div className="text-center font-bold text-3xl pb-5">Admin Tools</div>
+        <div className="flex flex-wrap max-w-[90vw] justify-center items-center gap-5">
+          <Link
+            href="/components/AdminView/Dashboard"
+            onClick={() => setOpen(false)}
+            className="bg-red-600 text-lg hover:bg-red-700 hover:scale-105 duration-200 py-2 px-3 text-white font-bold rounded-xl"
+          >
+            Dashboard
+          </Link>
+          <Link
+            href="/components/AdminView/AvailableProducts"
+            onClick={() => setOpen(false)}
+            className="bg-red-600 text-lg hover:bg-red-700 hover:scale-105 duration-200 py-2 px-3 text-white font-bold rounded-xl"
+          >
+            Products
+          </Link>
+          <Link
+            href="/components/AdminView/AvailableProducts"
+            onClick={() => setOpen(false)}
+            className="bg-red-600 text-lg hover:bg-red-700 hover:scale-105 duration-200 py-2 px-3 text-white font-bold rounded-xl"
+          >
+            Categories
+          </Link>
+          <Link
+            href="/components/AdminView/Orders"
+            onClick={() => setOpen(false)}
+            className="bg-red-600 text-lg hover:bg-red-700 hover:scale-105 duration-200 py-2 px-3 text-white font-bold rounded-xl"
+          >
+            Orders
+          </Link>
         </div>
-      </Modal>
-    </div>
+      </div>
+    </Modal>
+    // </div>
   );
 }
