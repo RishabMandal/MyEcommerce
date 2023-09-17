e76724443c58f502f98efd81f1a5874c1669bc64
Rishab Mandal <102305005+RishabMandal@users.noreply.github.com>
2023-09-17 11:57:39 +0530

    General improvements and some animations

--- src/app/components/Products/Categories/page.jsx
+++ src/app/components/Products/Categories/page.jsx
@@ -51,14 +51,6 @@ const page = () => {
               >
                 Go to sale
               </div>
-              {/* <div
-                onClick={() =>
-                  window.location.replace("https://www.apple.com/in/")
-                }
-                className="cursor-pointer rounded-xl p-3 text-base font-bold bg-white hover:bg-gray-300 duration-200 ease-in-out text-black"
-              >
-                Coming Soon
-              </div> */}
             </div>
           </motion.div>
           <motion.img
