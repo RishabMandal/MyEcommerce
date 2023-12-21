864409509492622de441a4f348b4e6868391ab19
Rishab Mandal <102305005+RishabMandal@users.noreply.github.com>
2023-12-21 09:38:16 +0530

    Coupons added, still under development

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import Navbar from "@/components/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Footer from "@/components/Footer";
-import ChatBot from "@/components/ChatBot/ChatBot";
+// import ChatBot from "@/components/ChatBot/ChatBot";
 
 const inter = Inter({ subsets: ["latin"] });
 
